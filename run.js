d3fec9e8387f20a25b685241e20fd5466a856c66
MarkAlexI <markovaleksandr108@gmail.com>
2022-06-08 19:41:03 +0300

    Add some visualisation for executable code.

--- run.js
+++ run.js
@@ -158,6 +158,39 @@ topEnv["print"] = function (value) {
   //return value;
 };
 
+run("do(define(total, 0),",
+  " define(count, 1),",
+  " while(<(count, 11),",
+  " do(define(total, +(total, count)),",
+  " define(count, +(count, 1)))),",
+  " print(total))");
+
+run("do(define(plusOne, fun(a, +(a, 1))),",
+  " print(plusOne(10)))");
+
+run("do(define(pow, fun(base, exp,",
+  " if(==(exp, 0),",
+  " 1,",
+  " *(base, pow(base, -(exp, 1)))))),",
+  " print(pow(2, 10)))");
+
+run("do(print(element(array(1, 2, 3), 1)))");
+
+run("do(define(sum, fun(array,",
+  " do(define(i, 0),",
+  " define(sum, 0),",
+  " while(<(i, length(array)),",
+  " do(define(sum, +(sum, element(array, i))),",
+  " define(i, +(i, 1)))),",
+  " sum))),",
+  " print(sum(array(1, 2, 3))))");
+
+run("do(define(f, fun(a, fun(b, +(a, b)))),",
+  " print(f(4)(5)))");
+
+//const tasks = document.getElementById("input").innerText;
+//eval(tasks);
+
 /*Do it again!©Appolo440*/
 /*  let prog = parse("if(true, false, true)"); 
   console.log(evaluate(prog, topEnv)); // → false
@@ -165,43 +198,8 @@ topEnv["print"] = function (value) {
   console.log(JSON.stringify(parse("+(a, 10)")));*/
   // → {type: "apply",
   // operator: {type: "word", name: "+"},
-  // args: [{type: "word", name: "a"},
+  // args: [{type: "word", name: "a"},=
   // {type: "value", value: 10}]}
-
-  run("do(define(total, 0),",
-    " define(count, 1),",
-    " while(<(count, 11),",
-    " do(define(total, +(total, count)),",
-    " define(count, +(count, 1)))),",
-    " print(total))");
-// → 55
-
-run("do(define(plusOne, fun(a, +(a, 1))),",
-    " print(plusOne(10)))");
-// → 11
-
-run("do(define(pow, fun(base, exp,",
-    " if(==(exp, 0),",
-    " 1,",
-    " *(base, pow(base, -(exp, 1)))))),",
-    " print(pow(2, 10)))");
-// → 1024
-
-run("do(print(element(array(1, 2, 3), 1)))");
-
-run("do(define(sum, fun(array,",
-    " do(define(i, 0),",
-    " define(sum, 0),",
-    " while(<(i, length(array)),",
-    " do(define(sum, +(sum, element(array, i))),",
-    " define(i, +(i, 1)))),",
-    " sum))),",
-    " print(sum(array(1, 2, 3))))");
-// → 6
-
-run("do(define(f, fun(a, fun(b, +(a, b)))),",
-    " print(f(4)(5)))");
-// → 9
 
 //verify exist of comments
 //run("do(print('# hello\n1'))");
